1d115b76d5a58326e7d01dcd7a8c237cf70dd265
Jon Dunaway <jdunaway@brightree.com>
2023-03-30 13:58:46 -0400

    Add project files.

---
+++ AndroidApp1/MainActivity.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.App;
+using AndroidX.Fragment.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Content.Res;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System.Threading;
+using AndroidX.DrawerLayout.Widget;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Embedding;
+using Microsoft.Maui.Controls.Hosting;
+using Telerik.Maui.Controls;
+using Telerik.Maui.Controls.Compatibility;
+using RadComboBoxSample;
+
+namespace AndroidApp1
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+
+                var builder = MauiApp.CreateBuilder();
+                builder.UseMauiEmbedding<App>();
+                builder.UseMauiApp<App>()
+                    .UseTelerik();
+
+                var mauiApp = builder.Build();
+
+                var mauiContext = new MauiContext(mauiApp.Services, this);
+
+                var mainPage = new MainPage();
+
+                var mauiView = mainPage.ToPlatform(mauiContext);
+
+                SetContentView(mauiView);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ RadComboBoxSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RadComboBoxSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
